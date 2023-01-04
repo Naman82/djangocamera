84ad40df1df00af2a03a1f47d0f4f45bc2b5a70d
Naman82 <b520031@iiit-bh.ac.in>
2023-01-04 19:15:05 +0530

    setup added

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
     packages=find_packages(),
     install_requires=['opencv-python', 'django'],
-    keywords=['django','python', 'video', 'stream', 'video stream', 'camera stream'],
+    keywords=['django','python', 'video', 'stream', 'video stream', 'camera stream','django camera','django video'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
